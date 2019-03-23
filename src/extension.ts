4828648146246f26e984db47084031b0b1a5354f
litefeel <litefeel@gmail.com>
2019-03-23 22:18:36 +0800

    add configuration for shaderlabformatter.indentation.conditionMacro

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { print } from 'util';
-import { ProviderResult } from 'vscode';
-import { open } from 'inspector';
 import { Indent } from './indent';
 
 
@@ -15,10 +12,17 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     // console.log('Congratulations, your extension "shaderlabformatter" is now active!');
 
+    enum MacroIndentation {
+        Dont = "dont",
+        Indent = "indent",
+    }
 
-    const LEFT_BRACKET = /#if|\{(?!})/;
-    const RIGHT_BRACKET = /#endif|(?<!{)\}/;
-    const TEMP_LEFT_BRACKET = /#else|#elif/;
+    const MACRO_BEGIN = /^\s*#if/;
+    const MACRO_END = /^\s*#endif/;
+    const MACRO_MIDDLE = /^\s*(#else|#elif)/;
+    const BRACKET_LEFT = /\{(?!})/;
+    const BRACKET_RIGHT = /(?<!{)\}/;
+
     let indentUtil: Indent = new Indent();
 
     function isComment(line: string) {
@@ -30,6 +34,8 @@ export function activate(context: vscode.ExtensionContext) {
         provideDocumentFormattingEdits(document: vscode.TextDocument, options, token) {
 
             indentUtil.initIndent(options.insertSpaces, options.tabSize);
+            let config = vscode.workspace.getConfiguration("shaderlabformatter.indentation");
+            let macroIndentation = config.get<MacroIndentation>("conditionMacro", MacroIndentation.Indent);
 
             // vscode.window.showInformationMessage('Hello World!');
             const result: vscode.TextEdit[] = [];
@@ -41,27 +47,43 @@ export function activate(context: vscode.ExtensionContext) {
                     continue;
                 }
                 const lineText = line.text;
-                const increaseIndent = LEFT_BRACKET.test(lineText);
-                const decreaseIndent = RIGHT_BRACKET.test(lineText);
-                const decreaseIndentOnlyOne = TEMP_LEFT_BRACKET.test(lineText);
-                if (!increaseIndent && decreaseIndent) {
+                const bracketLeft = BRACKET_LEFT.test(lineText);
+                const bracketRight = BRACKET_RIGHT.test(lineText);
+                const macroBegin = MACRO_BEGIN.test(lineText);
+                const macroEnd = MACRO_END.test(lineText);
+
+                const macroMiddle = MACRO_MIDDLE.test(lineText);
+                if (!bracketLeft && bracketRight) {
                     indent--;
                 }
-                if (decreaseIndentOnlyOne) {
-                    indent--;
+                let nowIndent = indent;
+                if (macroEnd || macroMiddle || macroBegin) {
+                    switch (macroIndentation) {
+                        case MacroIndentation.Dont:
+                            nowIndent = 0;
+                            break;
+                        case MacroIndentation.Indent:
+                            if (macroEnd) {
+                                indent--;
+                                nowIndent = indent;
+                            } else if (macroMiddle) {
+                                nowIndent = indent - 1;
+                            }
+                            break;
+                    }
                 }
 
                 var firstCharIdx = line.firstNonWhitespaceCharacterIndex;
-                if (!indentUtil.isIndent(lineText, firstCharIdx, indent)) {
+                if (!indentUtil.isIndent(lineText, firstCharIdx, nowIndent)) {
                     var pos = new vscode.Position(lineIdx, 0);
                     result.push(vscode.TextEdit.delete(new vscode.Range(lineIdx, 0, lineIdx, firstCharIdx)));
-                    result.push(vscode.TextEdit.insert(line.range.start, indentUtil.getIndent(indent)));
+                    result.push(vscode.TextEdit.insert(line.range.start, indentUtil.getIndent(nowIndent)));
                 }
 
-                if (increaseIndent && !decreaseIndent) {
+                if (bracketLeft && !bracketRight) {
                     indent++;
                 }
-                if (decreaseIndentOnlyOne) {
+                if (macroBegin && macroIndentation === MacroIndentation.Indent) {
                     indent++;
                 }
             }
